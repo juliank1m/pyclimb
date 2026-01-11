46c99f25c84772be4a573d36bf57462249984584
Julian Kim <juliankim4321@gmail.com>
2026-01-11 14:45:51 -0500

    Add problem listing and detail views with corresponding templates

--- problems/views.py
+++ problems/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Problem
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = "problems/index.html"
+    context_object_name = "published_problem_list"
+    
+    def get_queryset(self):
+        return Problem.objects.filter(is_published=True).order_by("pk")
+
+class DetailView(generic.DetailView):
+    model = Problem
+    template_name = "problems/detail.html"
+
+    def get_queryset(self):
+        return Problem.objects.filter(is_published=True)
